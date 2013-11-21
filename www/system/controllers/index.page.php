02a1c04e1a5c4bb61eae2d9c18e774b860b3c4ec
Fernando Val <fernando.val@gmail.com>
2013-11-21 12:50:53 +0000

    Atualização do framework para nova versão 1.3; Alteração da classe DB para usar PDO; Depreciação da adoDB; Migração para o framework Javascript jQuery e depreciação do framework Javascript Prototype;

---
+++ www/system/controllers/index.page.php
@@ -0,0 +1,37 @@
+<?php
+/**	\file
+ * 	\brief		Controladora da página principal da aplicação
+ *
+ *	\ingroup	controllers
+ *	\copyright	Copyright (c) 2007-2013 FVAL Consultoria e Informática Ltda.\n
+ *  \author		Fernando Val  - fernando.val@gmail.com
+ */
+
+class Index_Controller {
+	/**
+	 *  \brief Método construtor da controladora.
+	 *
+	 *  Este método sempre é executado quando a controladora é criada.
+	 *  Tudo que precisa se inicializadou e/ou instanciado com a conroladora deve ser colocado nesse método.
+	 */
+	function __construct() {
+		date_default_timezone_set('America/Sao_Paulo');
+	}
+
+	/**
+	 *  \brief Método principal (default)
+	 *
+	 *  Este método é executado se nenhum outro método for definido na URI para ser chamado, quando essa controladora é chamada.
+	 */
+	function _default() {
+		$date = date('F j, Y');
+		
+		Kernel::debug('Exemplo de debug 1');
+		Kernel::debug('Exemplo de debug 2', 'Exemplo com título');
+		Kernel::debug('Exemplo de debug 3', 'Título do Exemplo 3', false, false);
+
+		$tpl = new Template;
+		$tpl->assign('date', $date);
+		$tpl->display();
+	}
+}
