5031aa81dd3f2c02eea7d7ac630df57b1d878901
Fernando Val <fernando.val@gmail.com>
2016-04-17 16:13:26 -0300

    Removed trailing spaces from the end of a line because StyleCI show this as a CRITICAL issue.

--- tests/Core/CopyrightTest.php
+++ tests/Core/CopyrightTest.php
@@ -5,7 +5,7 @@
  *	\brief      Test case for the class Springy\Copyright.
  *  \copyright  (c) 2016 Fernando Val.
  *  \author     Fernando Val - fernando.val@gmail.com
- *	\version    0.1.0.1
+ *	\version    0.1.0.2
  *	\ingroup    tests
  */
 use Springy\Core\Copyright;
@@ -15,7 +15,7 @@ class CopyrightTest extends PHPUnit_Framework_TestCase
     public function testPrintCopyright()
     {
         $this->expectOutputRegex('/^<!DOCTYPE html>\n.+$/m');
-        
+
         $copyright = new Copyright(false);
         $copyright->printCopyright();
     }
