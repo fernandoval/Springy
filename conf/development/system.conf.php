fcf715c9a868a910c355932840601dd844e86e95
Fernando Val <fernando.val@gmail.com>
2022-04-17 15:24:09 -0300

    Removes uncecessary comments

--- conf/development/system.conf.php
+++ conf/development/system.conf.php
@@ -1,26 +1,4 @@
 <?php
-/**
- * General system configutations.
- *
- * Development environment.
- *
- * @copyright 2007 Fernando Val
- * @author    Fernando Val <fernando.val@gmail.com>
- */
-
-/**
- *  \defgroup systemcfg_development Configurações do cerne para o ambiente \c 'development'
- *  \ingroup systemcfg.
- *
- *  As entradas colocadas nesse arquivo serão aplicadas apenas ao ambiente 'development'.
- *
- *  Seu sistema pode não possuir esse ambiente, então use-o como modelo para criação do arquivo de
- *  parâmetros de configuração para os ambientes que seu sistema possua.
- *
- *  Veja \link systemcfg Configurações do cerne \endlink para entender as entradas de configuração possíveis.
- *
- *  \see systemcfg
- */
 
 $conf = [
     'debug' => true,
